f320c3a56d2f672156d783855955cc078e9efeed
David Lee <concoctionfactory@gmail.com>
2020-09-26 13:36:29 -0400

    changed config file

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-require("dotenv");
+require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "development-secret-key";
 
@@ -6,10 +6,18 @@ const PORT = process.env.PORT || 5000;
 
 const BCRYPT_WORK_FACTOR = 10;
 
-const DB_URI =
-  process.env.NODE_ENV === "test"
-    ? "postgresql:///workflow_test"
-    : "postgresql:///workflow";
+// const DB_URI =
+//   process.env.NODE_ENV === "test"
+//     ? "postgresql:///workflow_test"
+//     : "postgresql:///workflow";
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "workflow-test";
+} else {
+  DB_URI = process.env.DATABASE_URL || "workflow";
+}
 
 module.exports = {
   BCRYPT_WORK_FACTOR,
